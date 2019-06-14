35b60b246f6bd8967b85a042ef863d0228dadb03
sumeet rohra <sumeetrohra1@gmail.com>
2019-06-15 00:32:18 +0530

    updated git ignore

---
+++ src/firebase/index.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyByZy16hiEjq9kr9CtIMONaojArvZzsptk",
+    authDomain: "snake-game-3516e.firebaseapp.com",
+    databaseURL: "https://snake-game-3516e.firebaseio.com",
+    projectId: "snake-game-3516e",
+    storageBucket: "snake-game-3516e.appspot.com",
+    messagingSenderId: "611290672085",
+    appId: "1:611290672085:web:3faceae83e9844b6"
+};
